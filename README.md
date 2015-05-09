167f9a4178eeb8c6e5d8c4fa11305c824d5f81c4
bbmoz <blueberrymozart@gmail.com>
2015-05-09 12:53:25 -0500

    skeleton

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 <h1>Comet</h1>
+Spawn comets that can consume DOM elements and can react to the "gravitational" pulls of these elements.
+
+Despite this being a fun simulation, it conveys the color scheme of a specific web page as well as greater emphasis placed on certain DOM elements over others.
+
+<h2>TODO</h2>
+- Spawn comet in DOM (user clicks location, drags for initial velocity)
+- Comet consumes colliding elements
+  - grows in size but also slows down (comet can eventually stop due to a slow speed and opposing forces of "gravity" from different elements)
+  - comet trail becomes more colorful based off the consumed elements' colors
+- Comet is attracted by "large" elements and moves automatically
+- Multiple comets can be spawned
+- User can right click on a comet to remove it from the DOM
--- comet.js
+++ comet.js
@@ -1,3 +1,86 @@
-(function comet() {
+(function cometSpawner() {
+  var spawnedComets = [];
 
+  function Comet(startPos, startSpeed, startAngle) {
+    this.startPos = startPos;
+    this.startSpeed = startSpeed;
+    this.startAngle = startAngle;
+  }
+
+  Comet.prototype.spawn = function spawn() {
+    var cometInDom = document.createElement('comet');
+    cometInDom.addEventListener('collision', function () {
+
+    });
+    spawnedComets.push(this);
+  };
+
+  Comet.prototype.die =  function die() {
+    for (let cometIndex in spawnedComets) {
+      var comet = spawnedComets[cometIndex];
+      if (comet.startPos === this.startPos &&
+          comet.length = this.length &&
+          comet.startAngle = this.startAngle) {
+        spawnedComets.splice(cometIndex);
+      }
+    }
+  };
+
+  Comet.prototype.consume = function consume() {
+
+  };
+
+  Comet.prototype.move = function move() {
+    
+  };
+
+  function createNewComet(startPos, length, startAngle) {
+    var newComet = new Comet(startPos, length, startAngle)
+    newComet.spawn();
+  }
+
+  function getLengthBetween(mouseStartPos, mouseEndPos) {
+    var length = Math.sqrt(
+      (mouseStartPos.x * mouseStartPos.x + mouseStartPos.y * mouseStartPos.y) -
+      (mouseEndPos.x * mouseEndPos.x + mouseEndPos.y * mouseEndPos.y)
+    );
+    return length;
+  }
+
+  function getAngleOf(mouseStartPos, mouseEndPos) {
+    var radians = Math.inverseTan(
+      (mouseEndPos.y - mouseStartPos.y) / (mouseEndPos.x - mouseEndPos.x)
+    );
+    return radians;
+  }
+
+  (function addUserEventListeners() {
+    var $body = document.getElementByTagName('body')[0],
+        keysPressed = false,
+        mouseStartPos, mouseEndPos;
+
+    $body.addEventListener('keypress', function (e) {
+      var keysPressed = e.sources;
+      if (keysPressed.indexOf('fn') !== -1 &&
+          keysPressed.indexOf('window/command') !== -1 &&
+          keysPressed.indexOf('c')) {
+        keysPressed = true;
+      }
+    });
+    $body.addEventListener('click', function (e) {
+      var mousePos = e.source.position;
+      if (keysPressed) {
+        mouseStartPos = mousePos;
+      }
+    });
+    $body.addEventListener('unclick', function (e) {
+      var mousePos = e.source.position;
+      if (keysPressed) {
+        mouseEndPos = mousePos;
+        var length = getLengthBetween(mouseStartPos, mouseEndPos);
+        var startAngle = getAngleOf(mouseStartPos, mouseEndPos);
+        createNewComet(mouseStartPos, length, startAngle);
+      }
+    });
+  }());
 }());
